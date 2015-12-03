4e772b79aebe2935186e80183e1aafda7f4e2480
K <karolinaburska9@gmail.com>
2015-12-03 18:55:09 +0100

    styling added

--- db/migrate/20151201174839_create_posts.rb
+++ db/migrate/20151201174839_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :user_id
-      t.string :title
-      t.text :body
+      t.integer :user_id,  null: false
+      t.string :title,     null: false
+      t.text :body,        null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema.define(version: 20151202165803) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "body"
+    t.integer  "user_id",    null: false
+    t.string   "title",      null: false
+    t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
