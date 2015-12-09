bc54840e370d8a9ac6c4063cc16ae40662bb7f61
K <karolinaburska9@gmail.com>
2015-12-09 19:51:08 +0100

    email settings edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,21 +18,20 @@ Rails.application.configure do
 
   #config.action_mailer.default_url_options = { :host => 'ruby-hw.herokuapp.com' }
 
-   config.action_mailer.default_url_options = { :host => 'smtp.gmail.com' }
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'ruby-hw.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default :charset => "utf-8"
-   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "gmail.com",
+  config.action_mailer.smtp_settings = {
+      address: "smtp.seznam.cz",
+      port: 25,
+      domain: "seznam.cz",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
       password: ENV["GMAIL_PASSWORD"]
   }
-
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
